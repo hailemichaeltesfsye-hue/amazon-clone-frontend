d6a8351d3dfd6e05b5c02f1579868a39755d930b
hailemichael tesfaye <your.hailemichaeltesfsye@gmail.com>
2026-09-14 02:54:32 +0300

    Remove unused user variable in App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Type } from './Utility/action.type'
 import { auth } from './Utility/firebase'
 
 function App() {
-  const [{ user }, dispatch] = useContext(DataContext)
+  const [, dispatch] = useContext(DataContext)
 
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
